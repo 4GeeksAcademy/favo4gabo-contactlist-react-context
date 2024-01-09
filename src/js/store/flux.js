30dce2e708db48be75ec34b02f6601442f23b409
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2024-01-09 20:37:51 +0000

    falta el map

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,29 +1,26 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contact: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			
+			getContacts: () => {
+				
+				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/Gabriel")
+				.then((response) => response.json())
+				.then((data) => {
+					let store = getStore()
+					setStore({...store, contact:data})
+					console.log(data)
+				})
+				
 			},
+
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useEffect, useContext} from "react";
+import { Context } from "../store/appContext";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { useActionData } from "react-router";
 
-export const Home = () => (
+export const Home = () => {
+	const {store, actions} = useContext(Context)
+
+	useEffect(() => {actions.getContacts()}, [])
+	console.log(store.contact)
+
+	return(
+		 
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+	<div className="card mb-3 my-style">
+<div className="row g-0">
+<div className="col-md-4">
+  <img src="https://picsum.photos/200/300" className="img-fluid rounded-start" alt="..."/>
+</div>
+<div className="col-md-8">
+  <div className="card-body">
+	<h5 className="card-title">Card title</h5>
+	<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+</div>
+</div>
+</div>
+)
+};
