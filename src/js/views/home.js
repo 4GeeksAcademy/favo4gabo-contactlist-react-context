2232108db28cf644c81aeb7827990abf9fb407bc
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2024-01-09 21:12:42 +0000

    prueba del map

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -10,23 +10,27 @@ export const Home = () => {
 	useEffect(() => {actions.getContacts()}, [])
 	console.log(store.contact)
 
-	return(
+	{contact.map((index) => {
+		return(
 		 
-	<div className="text-center mt-5">
-	<div className="card mb-3 my-style">
-<div className="row g-0">
-<div className="col-md-4">
-  <img src="https://picsum.photos/200/300" className="img-fluid rounded-start" alt="..."/>
-</div>
-<div className="col-md-8">
-  <div className="card-body">
-	<h5 className="card-title">Card title</h5>
-	<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-	<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-</div>
-</div>
-</div>
-</div>
-)
+			<div className="text-center mt-5">
+			<div className="card mb-3 my-style">
+		<div className="row g-0">
+		<div className="col-md-4">
+		  <img src="https://picsum.photos/200/300" className="img-fluid rounded-start" alt="..."/>
+		</div>
+		<div className="col-md-8">
+		  <div className="card-body">
+			<h5 className="card-title">Card title</h5>
+			<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+		  </div>
+		</div>
+		</div>
+		</div>
+		</div>
+		
+		)
+	})}
+	
 };
